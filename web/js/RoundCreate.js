25b4762e44228210f8a8be8c8d13350ccd016e35
debart-nathan <denathan@laposte.net>
2022-07-05 12:55:37 +0200

    #10 : lisibility amelioration

--- web/js/RoundCreate.js
+++ web/js/RoundCreate.js
@@ -4,11 +4,11 @@
 *
 *
 */
-var jsonLevel;
+let jsonLevel;
 
 
 function allDiviser() {
-	var prop = document.getElementById("propositions");
+	const prop = document.getElementById("propositions");
 
 	prop.innerHTML="";
 
@@ -16,30 +16,30 @@ function allDiviser() {
 		prop.innerHTML = "aucune equipe";
 		return
 	}
-	var nb = jsonLevel.teams.length;
+	const nb = jsonLevel.teams.length;
 
 	if (nb == 1) {
-		document.prop.innerHTML = "qu'un participant le gagnant par default est donc " + jsonLevel.teams.name
+		document.prop.innerHTML = `qu'un participant le gagnant par default est donc ${jsonLevel.teams.name}`
 		return
 	}
-	var ulpr = document.createElement("ul")
+	const ulpr = document.createElement("ul")
 	ulpr.id = "ulpr"
 	prop.appendChild(ulpr)
 
-	var divise
+	let divise
 
 	if (nb == 2) {
-		divise = createRadio(1 + "x" + 2);
+		divise = createRadio(`1x2`);
 		ulpr.appendChild(divise);
 	}
 	for (let i = 1; i <= (nb / 2); i++) {						//Recherche de l'intégralité des diviseurs du nombre total d'équipes
 		ulpr.innerHTML += "<br>"
 		if (nb % i == 0) {
-			divise = createRadio(i + "x" + nb / i);
+			divise = createRadio(`${i}x${nb / i}`);
 			ulpr.appendChild(divise);							//Récupération du diviseur et du quotion qui correspondent au nombre de poules ainsi qu'au 	nombre d'équipes par poule		
 		} else {
-			var rst = nb % i;									//recuperation du reste de la partie entière de la division du nbEquipe par l'iteratuer i
-			var quot = Math.trunc(nb / i);						//recuperation de la partie entière de la division du nbEquipe par l'iterateur i
+			const rst = nb % i;									//recuperation du reste de la partie entière de la division du nbEquipe par l'iteratuer i
+			const quot = Math.trunc(nb / i);						//recuperation de la partie entière de la division du nbEquipe par l'iterateur i
 			strangePoule(quot, i, rst, ulpr);  					//on apel la fontion de création en cas de poule composé pour i poules de quot equipe
 			strangePoule(i, quot, rst, ulpr);
 		}
@@ -48,13 +48,13 @@ function allDiviser() {
 
 
 	prop.innerHTML += "<br>"
-	var lsetganiant = document.createElement("label")
+	const lsetganiant = document.createElement("label")
 	lsetganiant.for = "setganiant"
 	lsetganiant.innerHTML = "Nombre(s) de set(s) pour gagner un match  :"
 	prop.appendChild(lsetganiant)
 
 	prop.innerHTML += "<br>"
-	var setganiant = document.createElement("input")
+	const setganiant = document.createElement("input")
 	setganiant.type = "number"
 	setganiant.min = "1"
 	setganiant.id = "setganiant"
@@ -62,13 +62,13 @@ function allDiviser() {
 
 	prop.innerHTML += "<br>"
 	prop.innerHTML += "<br>"
-	var lpointganiant = document.createElement("label")
+	const lpointganiant = document.createElement("label")
 	lpointganiant.for = "pointganiant"
 	lpointganiant.innerHTML = "Nombre(s) de point(s) pour gagner un set  :"
 	prop.appendChild(lpointganiant)
 
 	prop.innerHTML += "<br>"
-	var pointganiant = document.createElement("input")
+	const pointganiant = document.createElement("input")
 	pointganiant.type = "number"
 	pointganiant.min = "1"
 	pointganiant.id = "pointganiant"
@@ -76,7 +76,7 @@ function allDiviser() {
 
 
 	prop.innerHTML += "<br>"
-	var erreur = document.createElement("p")
+	const erreur = document.createElement("p")
 	erreur.type = "ErrorText"
 	erreur.id = "erreur"
 	erreur.innerHTML = ""
@@ -84,7 +84,7 @@ function allDiviser() {
 
 
 	prop.innerHTML += "<br>"
-	var valide = document.createElement("button")
+	const valide = document.createElement("button")
 	valide.type = "button"
 	valide.setAttribute('onclick', "createPoule()")
 	valide.innerHTML = "validé"
@@ -93,22 +93,20 @@ function allDiviser() {
 
 
 function strangePoule(nbp, nbq, rst, prop) {
-	var opt = false;
-	var j = 0;
+	let opt = false;
 	while (!(opt) && nbp != 0) {
 		if (rst % (nbq - 1) == 0 && (nbq - 1) != 1) {
 			opt = true;
 		}
 		else {
-			j++;
 			nbp = nbp - 1;
 			rst = rst + nbq;
 		}
 	}
 
 	if (opt) {
-		var radio = createRadio(nbp + "x" + nbq + "+" + (rst / (nbq - 1)) + "x" + (nbq - 1));
-		if (prop.querySelector("[id='" + radio.id + "']") == null) {
+		const radio = createRadio(`${nbp}x${nbq}+${rst / (nbq - 1)}x${nbq - 1}`);
+		if (prop.querySelector(`[id='${radio.id}']`) == null) {
 			prop.appendChild(radio);
 
 		}
@@ -117,23 +115,23 @@ function strangePoule(nbp, nbq, rst, prop) {
 
 
 function createRadio(text) {
-	var li = document.createElement("le")
-	li.id = text + "-li"
+	const li = document.createElement("le")
+	li.id = `${text}-li`
 
-	var radio = document.createElement("input")
+	const radio = document.createElement("input")
 	radio.type = "radio"
 	radio.id = text
 	radio.name = "formule"
 	li.appendChild(radio)
 
-	var label = document.createElement("label")
-	label.id = text + "-lab"
+	const label = document.createElement("label")
+	label.id = `${text}-lab`
 	label.htmlFor = text
-	var ltext = text.split(/[x+]+/)
-	label.innerHTML = ltext[0] + " Poules de " + ltext[1] + " Equipes"
+	const ltext = text.split(/[x+]+/)
+	label.innerHTML = `${ltext[0]}  Poules de  ${ltext[1]}  Equipes`
 	if (ltext.length == 4) {
 
-		label.innerHTML += " et " + ltext[2] + " Poules de " + ltext[3] + " Equipes"
+		label.innerHTML += ` et  ${ltext[2]} Poules de ${ltext[3]} Equipes`
 
 	}
 
@@ -144,7 +142,7 @@ function createRadio(text) {
 
 
 function sortList(ulid) {
-	var list, i, switching, b, shouldSwitch;
+	let list, i, switching, b, shouldSwitch;
 	list = document.getElementById(ulid);
 	switching = true;
 	while (switching) {
@@ -152,8 +150,8 @@ function sortList(ulid) {
 		b = list.getElementsByTagName("LI");
 		for (i = 0; i < (b.length - 1); i++) {
 			shouldSwitch = false;
-			var parsedId1 = b[i].id.split(/[x+]+/);
-			var parsedId2 = b[i + 1].id.split(/[x+]+/);
+			const parsedId1 = b[i].id.split(/[x+]+/);
+			const parsedId2 = b[i + 1].id.split(/[x+]+/);
 			if (smaller(parsedId1, parsedId2)) {
 				shouldSwitch = true;
 				break;
@@ -183,9 +181,9 @@ function smaller(a, b) {
 
 }
 function echange(node1_id, node2_id) {
-	var node1 = document.getElementById(node1_id)
-	var node2 = document.getElementById(node2_id)
-	var tmp = document.createElement("span")
+	const node1 = document.getElementById(node1_id)
+	const node2 = document.getElementById(node2_id)
+	const tmp = document.createElement("span")
 	node1.parentNode.insertBefore(tmp, node1); 					 	//inserré l'élément temporaire avant la position actuel de node1
 	node2.parentNode.insertBefore(node1, node2);    				//place node1 juste avant node2
 	tmp.replaceWith(node2)											//remplace l'élément temporaire par node2 et le supprime à son ancienne position
@@ -196,12 +194,12 @@ function echange(node1_id, node2_id) {
 
 
 function createPoule() {
-	var formule = document.querySelector('input[name="formule"]:checked')
-	var setganiant = document.getElementById("setganiant").value
-	var pointganiant = document.getElementById("pointganiant").value
+	const formule = document.querySelector('input[name="formule"]:checked')
+	const setganiant = document.getElementById("setganiant").value
+	const pointganiant = document.getElementById("pointganiant").value
 
 
-	var erreurText = ""
+	const erreurText = ""
 
 	if (formule == null) {
 		erreurText += "<br> sélectioner une formule "
@@ -219,11 +217,11 @@ function createPoule() {
 	}
 
 	if (erreurText != "") {
-		var erreur = document.getElementById("erreur")
+		const erreur = document.getElementById("erreur")
 		erreur.innerHTML = erreurText
 		return
 	}
-	var tabFormule = formule.id.split("+")
+	let tabFormule = formule.id.split("+")
 
 
 	jsonLevel.setganiant = setganiant
@@ -232,9 +230,9 @@ function createPoule() {
 		jsonLevel.poule = new Array
 	}
 
-	for (var i = 0; i < tabFormule.length; i++) {
+	for (let i = 0; i < tabFormule.length; i++) {
 		tabFormule[i] = tabFormule[i].split("x");
-		for (var j = 0; j < tabFormule[i][0]; j++) {
+		for (let j = 0; j < tabFormule[i][0]; j++) {
 
 			jsonLevel.poule.push({ "nbequipe": tabFormule[i][1] })
 		}
@@ -245,10 +243,10 @@ function createPoule() {
 
 
 
-async function getJSON() {
+ async function getJSON() {
 
-	await fetch("./data/Level.JSON") //TODO : Replace by Get from server with arg {"tournoi": getElementById('tournoi').value,"Evenement": getElementById('round').value,"Event": getElementById('round').value}
-		.then(response => response.json()).then(data => jsonLevel = data).catch((error) => console.log(error));
+	jsonLevel =await (await fetch("./data/Level.JSON")).json() //TODO : Replace by Get from server with arg {"tournoi": getElementById('tournoi').value,"Evenement": getElementById('round').value,"Event": getElementById('round').value}
+		
 	allDiviser()
 }
 
@@ -268,16 +266,16 @@ getJSON()
 function repartTeams() {
 
 	repartSerpentin()
-	var root = document.getElementById("dynamique")
+	const root = document.getElementById("dynamique")
 	while (root.firstChild) {
 		root.removeChild(root.firstChild);
 	}
 
-	var dynTools = document.createElement("div")
+	const dynTools = document.createElement("div")
 	dynTools.id = "dynTools"
-	var poules = document.createElement("article")
+	const poules = document.createElement("article")
 	poules.id = "poules"
-	var dynNavigation = document.createElement("div")
+	const dynNavigation = document.createElement("div")
 	dynNavigation.id = "dynNavigation"
 
 	root.appendChild(dynTools)
@@ -286,7 +284,7 @@ function repartTeams() {
 
 
 	//[DYNTOOLS]
-	var resetbutton = document.createElement('button')
+	const resetbutton = document.createElement('button')
 	resetbutton.type = 'button'
 	resetbutton.innerHTML = "Reset Teams distribution"
 	resetbutton.onclick = resetTd
@@ -304,11 +302,11 @@ function repartTeams() {
 }
 
 function repartSerpentin() {
-	var team_index = 0
+	let team_index = 0
 	for (const poule of jsonLevel.poule) {
 		poule.teamIid= new Array()
 		poule.teamIid.length=poule.nbequipe
-		for (var i = 0; i < poule.nbequipe; i++) {
+		for (let i = 0; i < poule.nbequipe; i++) {
 			poule.teamIid[i]=team_index
 			team_index++
 		}
@@ -316,22 +314,22 @@ function repartSerpentin() {
 }
 
 function setPoules() {
-	var rootp = document.getElementById("poules")
+	const rootp = document.getElementById("poules")
 	while (rootp.firstChild) {
 		rootp.removeChild(rootp.firstChild);
 	}
-	for (var pi = 0; pi < jsonLevel.poule.length; pi++) {
-		var poule = document.createElement('ul')
-		poule.id = "p" + pi
+	for (let pi = 0; pi < jsonLevel.poule.length; pi++) {
+		const poule = document.createElement('ul')
+		poule.id = `p${pi}`
 		
 		
 		poule.value = pi
 		rootp.appendChild(poule)
-		for (var id=0;id<jsonLevel.poule[pi].teamIid.length;id++ ) {
-			var teamId=jsonLevel.poule[pi].teamIid[id];
-			var team = jsonLevel.teams[teamId]
-			var lit = document.createElement('li')
-			lit.id = "t" + team.inscrid
+		for (let id=0;id<jsonLevel.poule[pi].teamIid.length;id++ ) {
+			const teamId=jsonLevel.poule[pi].teamIid[id];
+			const team = jsonLevel.teams[teamId]
+			const lit = document.createElement('li')
+			lit.id = `t${team.inscrid}`
 			lit.innerHTML = team.name
 			lit.draggable=true;
 			lit.addEventListener("dragstart" ,drag )
